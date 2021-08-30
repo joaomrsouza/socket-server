0e0a6759ddabf10e8e54a785f899495376989a11
João Marcos <joaomarcos25@hotmail.com>
2021-08-29 21:37:19 -0300

    socket connection route

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const http = require('http').createServer();
+const express = require("express");
+const app = express();
+const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
+});
+
+app.get("/socket.io/", (req, res) => {
+  res.send(200);
 });
 
 io.on('connection', (socket) => {
